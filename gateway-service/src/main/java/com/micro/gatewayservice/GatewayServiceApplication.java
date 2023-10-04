1b2faf1325a87209d6f0ed5e5ac67e1a5fc7264c
Aabilla Said <said160aabilla@gmail.com>
2023-10-04 15:13:49 +0100

    dynamic routes config and registry with eureka

--- gateway-service/src/main/java/com/micro/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/micro/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package com.micro.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +16,13 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+    // dynamic routes config
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient client, DiscoveryLocatorProperties properties) {
+        return new DiscoveryClientRouteDefinitionLocator(client,properties);
+    }
+
+    // static routes config
    /* @Bean
     RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
