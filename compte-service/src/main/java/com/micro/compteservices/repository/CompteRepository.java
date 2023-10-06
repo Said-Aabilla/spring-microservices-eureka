ffb585ecaea3174e995d41fe20b7ef8bb08c8ee3
Aabilla Said <said160aabilla@gmail.com>
2023-10-06 14:22:52 +0100

    renamed account service

--- compte-service/src/main/java/com/micro/compteservices/repository/CompteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.micro.compteservices.repository;
-
-import com.micro.compteservices.entity.Compte;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompteRepository extends JpaRepository<Compte, Long> {
-}
