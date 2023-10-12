b99d190b78e413a00b0f6b3dd6111eb400fab6b0
Aabilla Said <said160aabilla@gmail.com>
2023-10-12 15:18:22 +0100

    removed account service

--- account-service/src/main/java/com/micro/compteservices/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.micro.accountservice.repository;
-
-import com.micro.accountservice.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
